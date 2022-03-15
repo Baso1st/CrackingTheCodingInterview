9082c27b3598fc219bf2218f869bca839330d594
Baso1st <abdulwanis1@gmail.com>
2022-03-15 17:30:13 +0200

    one complete solution for the one_away problem

--- 1.5_one_away.py
+++ 1.5_one_away.py
@@ -7,20 +7,56 @@ def one_away(first, second):
         return False
     
     if len(second) < len(first): #Insert a character
-        for i in range(len(first)):
+        for i in range(len(second)):
             if first[i] != second[i]:
                 second = second[:i] + first[i] + second[i:]  
-                if first == second:
-                    return True
-                else:
-                    return False
+                return first == second
+        second += first[-1]
+        return first == second
     elif len(second) > len(first): #Remove a character
-        pass
+        for i in range(len(first)):
+            if first[i] != second[i]:
+                second = second[:i] + second[i+1:]
+                return first == second
+        second = second[:-1]
+        return first == second    
     else: #Replace a character
-        pass
+        for i in range(len(first)):
+            if first[i] != second[i]:
+                second = second[:i] + first[i] + second[i+1:]
+                return first == second
+        return True
 
+test_cases = [
+    # no changes
+    ("pale", "pale", True),
+    ("", "", True),
+    # one insert
+    ("pale", "ple", True),
+    ("ple", "pale", True),
+    ("pales", "pale", True),
+    ("ples", "pales", True),
+    ("pale", "pkle", True),
+    ("paleabc", "pleabc", True),
+    ("", "d", True),
+    ("d", "de", True),
+    # one replace
+    ("pale", "bale", True),
+    ("a", "b", True),
+    ("pale", "ble", False),
+    # multiple replace
+    ("pale", "bake", False),
+    # insert and replace
+    ("pale", "pse", False),
+    ("pale", "pas", False),
+    ("pas", "pale", False),
+    ("pkle", "pable", False),
+    ("pal", "palks", False),
+    ("palks", "pal", False),
+    # permutation with insert shouldn't match
+    ("ale", "elas", False),
+]
 
-# first = 'abcdefg'
-# second = 'abcdef'
-# i = 6
-# print(second[:i] + first[i] + second[i:] )
+for case in test_cases:
+    if one_away(case[0], case[1]) != case[2]:
+        print(case)
