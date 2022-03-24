7d0fca91365b4674f38b995989ac20a56e8668e4
Baso1st <abdulwanis1@gmail.com>
2022-03-24 18:22:27 +0200

    A small refactor

--- 2.5_sum_lists.py
+++ 2.5_sum_lists.py
@@ -20,7 +20,7 @@ def sum_lists(head1: Node, head2: Node):
         carry = sum // 10
         result.next = Node()
         result = result.next
-        result.data = sum - (carry * 10)
+        result.data = sum % 10
         number1 = number1.next
         number2 = number2.next
 
@@ -31,6 +31,15 @@ def sum_lists(head1: Node, head2: Node):
     return resultHead
 
 
+def sum_lists_reversed(number1: Node):
+    for x in recurse(number1):
+        pass
+
+
+def recurse(node: Node):
+    if node.next is not None:
+        yield from recurse(node.next)
+    yield node.data
 
 ################## TEST_START ##############
 
@@ -38,11 +47,14 @@ def sum_lists(head1: Node, head2: Node):
 head1 = linked_list_from_list([7, 1, 6])
 head2 = linked_list_from_list([5, 9, 2])
 
+
+# print(sum_lists_reversed(head1))
+
 # head1 = linked_list_from_list([9, 7, 8])
 # head2 = linked_list_from_list([6, 8, 5])
 
-print_list(head1)
-print_list(head2)
+# print_list(head1)
+# print_list(head2)
 
 result = sum_lists(head1, head2)
 
