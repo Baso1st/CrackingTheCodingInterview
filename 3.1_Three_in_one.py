ed94bbe36b100a52321ac17493ebc4d33d4e578e
Baso1st <abdulwanis1@gmail.com>
2022-04-03 16:57:01 +0200

    Added a fixed implementation of 3 stacks using one array.

--- 3.1_Three_in_one.py
+++ 3.1_Three_in_one.py
@@ -1,28 +1,62 @@
+
+
 class Stack:
-    def __init__(self):
-        self.size = 0
-        self.arr = 20 * [None]
+    def __init__(self, arr, start, end) -> None:
+        self.arr = arr
+        self.start = start
+        self.end = end
+        self.count = 0
 
-
-    def _increase_array_size(self):
-        pass
-
-
+    
     def push(self, data):
-        if self.size == len(self.arr):
-            self._increase_array_size()
-        self.arr[self.size] = data
-        self.size += 1
-    
-    
+        if self.start + self.count == self.end:
+            raise Exception("The Stack is Full")
+        self.arr[self.start + self.count] = data
+        self.count += 1
+
+
     def pop(self):
-        temp = self.arr[self.size]
-        self.arr[self.size] = None
-        self.size -= 1
-        return temp  
-    
-    
-    def peek(self):
-        return self.arr[self.size]
+        if self.count == 0:
+            raise Exception("The Stack is Empty")
+        index = self.start + self.count - 1
+        data = self.arr[index]
+        self.arr[index] = None
+        self.count -= 1
+        return data
 
 
+
+def main():
+    arr = 30 * [None]
+    firstStack = Stack(arr, 0, 9)
+    secondStack = Stack(arr, 10, 19)
+    thirdStack = Stack(arr, 20, 29)
+
+    firstStack.push('FirstStackFirstItem')
+    firstStack.push('FirstStackSecondItem')
+    firstStack.push('FirstStackThirdItem')
+
+    secondStack.push('SecondStackFirstItem')
+    secondStack.push('SecondStackSecondItem')
+    secondStack.push('SecondStackThirdItem')
+
+    thirdStack.push('ThirdStackFirstItem')
+    thirdStack.push('ThirdStackSecondItem')
+    thirdStack.push('ThirdStackThirdItem')
+
+    print(firstStack.pop())
+    print(firstStack.pop())
+    print(firstStack.pop())
+
+    print(secondStack.pop())
+    print(secondStack.pop())
+    print(secondStack.pop())
+
+    print(thirdStack.pop())
+    print(thirdStack.pop())
+    print(thirdStack.pop())
+    
+    
+
+if __name__ == '__main__':
+    main()
