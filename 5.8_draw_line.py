08e091dd38fe31c1154b1e1acf863bf1622dbeb8
Baso1st <abdulwanis1@gmail.com>
2022-04-29 15:30:18 +0200

    An Implementation to 5.8_draw_line

---
+++ 5.8_draw_line.py
@@ -0,0 +1,29 @@
+
+
+def draw_line(screen: list, width, x1, x2, y):
+    if x2 - x1 > width  or x2 - x1 < 0:
+        raise Exception("Nope")
+    
+    widthInBytes = int(width / 8)
+    yBegins = y * widthInBytes
+
+    lineLength = (x2 - x1) + 1
+    base = (1 << lineLength) - 1 
+    ones = base << width - (x2 +1 )
+    bytes = ones.to_bytes(widthInBytes, byteorder='big')
+    
+    for idx, byte in enumerate(bytes):
+        screen[yBegins + idx] = byte
+
+
+    print(screen)
+    pass
+
+
+def main():
+    # draw_line(bytearray(4), 16, 0, 3, 1)
+    draw_line(bytearray(32), 64, 0, 63, 3)
+
+
+if __name__ == '__main__':
+    main()
