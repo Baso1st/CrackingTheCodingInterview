0b025340affed2c8714d19e155d8cbf68960355a
Baso1st <abdulwanis1@gmail.com>
2022-04-30 16:48:28 +0200

    another test case for 5.8

--- 5.8_draw_line.py
+++ 5.8_draw_line.py
@@ -9,20 +9,19 @@ def draw_line(screen: list, width, x1, x2, y):
 
     lineLength = (x2 - x1) + 1
     base = (1 << lineLength) - 1 
-    ones = base << width - (x2 +1 )
-    bytes = ones.to_bytes(widthInBytes, byteorder='big')
+    row = base << width - (x2 +1 )
+    bytes = row.to_bytes(widthInBytes, byteorder='big')
     
     for idx, byte in enumerate(bytes):
         screen[yBegins + idx] = byte
 
-
     print(screen)
-    pass
 
 
 def main():
     # draw_line(bytearray(4), 16, 0, 3, 1)
-    draw_line(bytearray(32), 64, 0, 63, 3)
+    # draw_line(bytearray(32), 64, 0, 63, 3)
+    draw_line(bytearray(32), 16, 0, 15, 1)
 
 
 if __name__ == '__main__':
